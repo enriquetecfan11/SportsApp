3fea29028ed3ed2ceccde12d1e3e615431f8cc5d
kikerodrivela <kikerodrivela@gmail.com>
2023-03-10 18:11:12 +0100

    starting goals screen

--- screens/GoalsScreen.js
+++ screens/GoalsScreen.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
 
 function GoalsScreen() {
+  const [goal, setGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const handleAddGoal = () => {
+    if (goal.trim()) {
+      setGoals([...goals, { id: Math.random().toString(), title: goal.trim() }]);
+      setGoal('');
+    }
+  };
+
+  const handleRemoveGoal = (id) => {
+    setGoals((currentGoals) => currentGoals.filter((goal) => goal.id !== id));
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bienvenido a Goals Screen</Text>
-      <Text style={styles.subtitle}>Empieza a establecer tus objetivos y hábitos ahora mismo!</Text>
+      <View style={styles.inputContainer}>
+        <FlatList
+          data={goals}
+          renderItem={({ item }) => (
+            <View style={styles.goal}>
+              <Text style={styles.goalText}>{item.title}</Text>
+              <TouchableOpacity onPress={() => handleRemoveGoal(item.id)}>
+                <Text style={styles.goalRemove}>X</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Agregar un objetivo"
+          value={goal}
+          onChangeText={(text) => setGoal(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleAddGoal}>
+          <Text style={styles.buttonText}>Agregar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
